f316b6241a96949ac827c6fda30420880bef1710
silicasandwhich <43558271+Silicasandwhich@users.noreply.github.com>
2021-02-28 16:11:17 -0500

    created install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+if [[ $HOME == "/root" ]]; then
+    echo "error: do not run this script as root!" >&2
+    exit 1
+fi
+echo -n "Would you like to install or uninstall? [I/u]: "
+read ans
+if [[ ${ans:0:1} == "u" || ${ans:0:1} == "U" ]]; then
+    echo "uninstalling"
+    echo -n "deleting cache..."
+    sudo rm -rf $HOME/.pyverman
+    echo "DONE"
+    echo -n "removing pyverman script..."
+    sudo rm /usr/bin/pyverman
+    echo "DONE"
+    echo "pyverman has been uninstalled."
+else
+    echo "installing"
+    echo -n "creating pyverman cache folder..."
+    sudo mkdir $HOME/.pyverman
+    echo "DONE"
+    echo -n "installing pyverman script..."
+    sudo cp bin/pyverman /usr/bin/pyverman
+    sudo chmod +x /usr/bin/pyverman
+    echo "DONE"
+    echo "Installation complete! use 'pyverman -h' for more information"
+fi
